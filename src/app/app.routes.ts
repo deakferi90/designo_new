c24635065c060c0799cca4eb510ed1e8d2258159
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-10-28 15:49:42 +0200

    made the routes in the header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { OurCompany } from './our-company/our-company';
+import { Locations } from './locations/locations';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'our-company',
+    component: OurCompany,
+  },
+  {
+    path: 'locations',
+    component: Locations,
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+  { path: '**', redirectTo: '/our-company' },
+];
---
+++ src/app/our-company/our-company.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-company',
+  imports: [],
+  templateUrl: './our-company.html',
+  styleUrl: './our-company.scss',
+})
+export class OurCompany {
+
+}
